d73c09aaee169834342ee2c07c3afdf740eaf58b
Andres <andresgalfajar@gmail.com>
2020-10-17 14:44:23 -0500

    Update some functions from the mechanics lab automator

--- mechanics/pi.py
+++ mechanics/pi.py
@@ -6,13 +6,13 @@ def main():
         n_list = line.split()
         n_list = [v.replace(",", ".") for v in n_list]
         n_list  = [float(n) for n in n_list]
+
         i = 0
-        
         while i < len(n_list):
             pi = n_list[i+4] / n_list[i]
             print("\n{} / {}".format(n_list[i+4],n_list[i]))
             print(round(pi, 2))
-            i += 1
+            i += 1   
             if i % 4 == 0:
                 i += 4
 
--- mechanics/prom.py
+++ mechanics/prom.py
@@ -3,7 +3,7 @@ def get_data():
     file_handler = open("1_parcial/pdata.txt")
     
     for line in file_handler:
-        value = line.split();
+        value = line.split()
         for n in value:
             acum += float(n)
 
